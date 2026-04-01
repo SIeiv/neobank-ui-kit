91a4de4a092943abb5e12e8edce4e4b57316d95a
SIeiv <maderator000@gmail.com>
2026-04-01 16:09:27 +0300

    setup storybook & add button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,23 @@
+import type { CSSProperties, ReactElement, ReactNode } from "react";
+
+import styles from "./button.module.css";
+
+interface IButton {
+  variation?: "primary" | "secondary";
+  type?: "default" | "flat" | "rounded";
+  children?: ReactNode | ReactNode[];
+  style?: CSSProperties
+}
+
+export const Button = ({
+  variation = "primary",
+  type = "default",
+  style,
+  children,
+}: IButton) => {
+  return (
+    <button style={style} className={`${styles.button} ${styles[variation]} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+import dts from "vite-plugin-dts";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
+  build: {
+    // Режим библиотеки
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), 
+      name: 'NeobankUiKit', 
+      fileName: (format) => `neobank-ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
